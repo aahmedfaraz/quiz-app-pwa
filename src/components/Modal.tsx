2a97e352163ae93a30e29eda07ecb7a2da8280a4
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-09-23 17:48:46 +0500

    Updated Modal Information

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,11 +15,17 @@ const Modal:React.FC = () => {
             </IconButton>
             <Dialog open={display} onClose={() => setDisplay(false)} className="modal">
                 <DialogTitle title="By Ahmed Faraz" style={{textAlign: 'center'}}>
-                    About Quiz App
+                    About Quiz App PWA
                 </DialogTitle>
                 <DialogContent dividers>
                     <Typography variant="body1" color="secondary">Developed by <strong>Ahmed Faraz</strong></Typography>
                     <Typography variant="body2" gutterBottom>Developed using <strong>React</strong> and <strong>Typescript</strong></Typography>
+                    <Typography variant="h5" color="primary">FEATURES</Typography>
+                    <ul>
+                        <li><Typography variant="body2"><i style={{color: "limegreen"}} className="fas fa-check"></i> Offline Availability</Typography></li>
+                        <li><Typography variant="body2"><i style={{color: "limegreen"}} className="fas fa-check"></i> Installable</Typography></li>
+                        <li><Typography variant="body2"><i style={{color: "limegreen"}} className="fas fa-check"></i> Push Notifications</Typography></li>
+                    </ul>
                     <Typography variant="h5" color="primary">TECH USED</Typography>
                     <ul>
                         <li><Typography variant="body2" color="secondary">React</Typography></li>
@@ -31,6 +37,7 @@ const Modal:React.FC = () => {
                         <li><Typography variant="body2">GitHub Actions</Typography></li>
                         <li><Typography variant="body2">Surge</Typography></li>
                     </ul>
+                <Typography variant="subtitle2">Now a <strong>Progressive Web App</strong>, use even with no internet connectivity and experiance like a <strong>Native Application</strong> with daily <strong>Push Notifications</strong>.</Typography>
                 </DialogContent>
                 <DialogActions>
                     <Button color="primary" onClick={() => setDisplay(false)}>
